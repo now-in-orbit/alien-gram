f9f97fa8dc68c130025b899f040392727c5cd60d
Imyraly Garcia <iarambula@cnm.edu>
2021-05-10 17:24:48 -0600

    added footer.js file

---
+++ frontend/src/ui/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <>
+            <footer className="container">
+                <img src="">
+            </footer>
+        </>
+    )
+}
