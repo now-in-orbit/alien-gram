52a822cd689f427938bffb20ec7766883624d10f
Robert Inez <rinez@cnm.edu>
2021-05-11 16:21:35 -0600

    Marty fixed navbar for us...

--- frontend/src/ui/components/NavBar.js
+++ frontend/src/ui/components/NavBar.js
@@ -8,15 +8,15 @@ export const  NavBar =  () => {
                 <Navbar.Brand href="#home">AlienGram</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto mr-5">
+                    <Nav className="ml-auto">
                         <Nav.Link className='navlink' href="#home">Home</Nav.Link>
-                        <NavDropdown title="Profile" id="basic-nav-dropdown" className='dropdown-menu-right'>
+                        <Nav.Link href="#link">Map</Nav.Link>
+                        <NavDropdown title="Profile" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.3">My Profile</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.1">Posts</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Transmissions</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Likes</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#link">Map</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
