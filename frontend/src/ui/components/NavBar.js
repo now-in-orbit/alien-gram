4688be52f52c52fbe8328304e7ea1c2a94843b82
Robert Inez <rinez@cnm.edu>
2021-05-11 15:14:50 -0600

    combined navbar and welcome

--- frontend/src/ui/components/NavBar.js
+++ frontend/src/ui/components/NavBar.js
@@ -1,21 +1,22 @@
 import React from "react"
 import {Nav, Navbar, NavDropdown} from "react-bootstrap"
+import '../Welcome.css'
 export const  NavBar =  () => {
     return (
         <>
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="dark" expand="lg" variant='dark'>
                 <Navbar.Brand href="#home">AlienGram</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Map</Nav.Link>
-                        <NavDropdown title="Profile" id="basic-nav-dropdown">
+                    <Nav className="ml-auto mr-5">
+                        <Nav.Link className='navlink' href="#home">Home</Nav.Link>
+                        <NavDropdown title="Profile" id="basic-nav-dropdown" className='dropdown-menu-right'>
                             <NavDropdown.Item href="#action/3.3">My Profile</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.1">Posts</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Transmissions</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Likes</NavDropdown.Item>
                         </NavDropdown>
+                        <Nav.Link href="#link">Map</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
