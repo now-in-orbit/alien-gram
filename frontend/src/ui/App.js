ac5caf794fb9a6974ede1e079a0fee3b7f035c20
Brody-code-designer <cslama@cnm.edu>
2021-05-12 13:55:27 -0600

    added a map page. added map component to router

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -6,6 +6,7 @@ import { Welcome } from './Welcome';
 import React from 'react';
 import {NavBar} from "./components/NavBar";
 import {Footer} from "./Footer";
+import {Map} from "./Map"
 
 export const App = () => (
 	<>
@@ -15,6 +16,7 @@ export const App = () => (
 				<Route exact path='/' component={Home} />
 				<Route exact path='/fouroffour' component={FourOhFour} />
 				<Route exact path='/welcome' component={Welcome} />
+				<Route exact path='/map' component={Map} />
 			</Switch>
 		</BrowserRouter>
 		<Footer />
---
+++ frontend/src/ui/Map.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export const Map = () => {
+    return (
+        <>
+            <h1>Map Placeholder</h1>
+        </>
+    )
+}
