0bb09617fc5589c71fca3b1011626ba6cc1595b9
Brody-code-designer <cslama@cnm.edu>
2021-05-18 16:28:41 -0600

    added an alien animation

---
+++ frontend/src/ufo-animation/Ufo.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './ufo-style.css'
+import UFOImg from './ufo-clipart.png'
+
+
+export const Ufo = () => {
+    return (
+        <>
+            <img src={UFOImg} className="UFO" alt="UFO" />
+        </>
+    )
+}
--- frontend/src/ui/Welcome.js
+++ frontend/src/ui/Welcome.js
@@ -1,20 +1,23 @@
 import React from "react";
 import './Welcome.css';
 import {Container, Row, Col} from 'react-bootstrap';
+import {Ufo} from "../ufo-animation/Ufo"
 
 
 export const Welcome = () => {
-	return (
-		<>
-			<Container className="jumbo-bg" fluid>
-				<Row>
-					<Col md={12}>
-						<h1>Welcome to AlienGram!</h1>
-						<br/>
-						<h2>Where you can connect and chat about extraterrestrial life</h2>
-					</Col>
-				</Row>
-			</Container>
-		</>
-	)
+    return (
+        <>
+            <Container className="jumbo-bg" fluid>
+                <Row>
+                    <Col md={12}>
+                        <h1>Welcome to AlienGram!</h1>
+                        <br/>
+                        <h2>Where you can connect and chat about extraterrestrial life</h2>
+                    </Col>
+                    <Ufo />
+                </Row>
+
+            </Container>
+        </>
+    )
 }
