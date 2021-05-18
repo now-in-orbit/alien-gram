5459a8f8d360b2ee7548bf98eb371557e79aa504
Jawaun Young <jyoung84@cnm.edu>
2021-05-18 15:51:30 -0700

    added nav content

--- frontend/src/ui/components/NavBar.js
+++ frontend/src/ui/components/NavBar.js
@@ -5,17 +5,17 @@ export const  NavBar =  () => {
     return (
         <>
             <Navbar bg="dark" expand="lg" variant='dark'>
-                <Navbar.Brand href="#home">AlienGram</Navbar.Brand>
+                <Navbar.Brand href="Home">AlienGram</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link className='navlink' href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Map</Nav.Link>
                         <NavDropdown title="Profile" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Posts</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Transmissions</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Likes</NavDropdown.Item>
+                            <NavDropdown.Item href="/Profile">My Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="Post">Posts</NavDropdown.Item>
+                            <NavDropdown.Item href="Transmissions">Transmissions</NavDropdown.Item>
+                            <NavDropdown.Item href="Likes">Likes</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
