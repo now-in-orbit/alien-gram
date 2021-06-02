f881e0521397524c00c400cfc4b58a1db7d69ef9
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-06-02 17:15:37 -0600

    added last sql enabled function

---
+++ backend/src/utils/profile/updateWholeProfileByProfileId.ts
@@ -0,0 +1,16 @@
+import {connect} from "../database.utils";
+import {Profile} from "../interfaces/Profile";
+
+export async function updateWholeProfileByProfileId(profile: Profile) {
+    try {
+
+        const mysqlConnection = await connect();
+        const query : string = 'update profile set profileActivationToken = :profileActivationToken, profileAvatarUrl = :profileAvatarUrl, profileEmail = :profileEmail, profileFirstName = :profileFirstName, profileHash = :profileHash, profileLastName = :profileLastName,profileUsername = :profileUsername where profileId = uuid_to_bin(:profileId)';
+
+        const [rows] = await mysqlConnection.execute(query, profile);
+        return 'Profile successfully updated'
+    } catch (e) {
+        console.error(e)
+        return null
+    }
+}
