c7ef2af3e8fdebe2bdd37ce6d901761da9666d2c
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-06-05 18:44:38 -0600

    removed statement

--- sql/statements.sql
+++
@@ -1,4 +0,0 @@
-# INSERT INTO profile (profileId, profileAboutMe, profileEmail, profileHash, profileName)
-# VALUES (UUID_TO_BIN("4864cdda-b95e-11eb-8529-0242ac130003"), "I'm a pro gamer and sreamer", "Lozzy@fatcash.com", "dskfjsdkfje342", "Lorenzo");
-
-INSERT INTO profile (profileId, profileActivationToken, profileAvatarUrl, profileEmail, profileFirstName, profileHash, profileLastName, profileUsername) VALUES (UUID_TO_BIN("4864cdda-b95e-11eb-8529-0242ac130003"),"blah", "blah2", "blah@gmail.com", "Lorenzo", "blah", "Oznerol", "AliensMyDude")
