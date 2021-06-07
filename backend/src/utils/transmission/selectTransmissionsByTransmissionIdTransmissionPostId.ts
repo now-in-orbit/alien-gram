31d55aa5ec4b32a3b655d1c870622b60d2319629
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-06-07 13:03:26 -0600

    fixed bug

--- backend/src/utils/transmission/selectTransmissionsByTransmissionIdTransmissionPostId.ts
+++ backend/src/utils/transmission/selectTransmissionsByTransmissionIdTransmissionPostId.ts
@@ -5,10 +5,7 @@ export async function selectTransmissionsByTransmissionIdTransmissionPostId(tran
     try {
         const mySqlConnection = await connect()
 
-        const [rows] = await mySqlConnection.execute('select bin_to_uuid(transmissionId) as transmissionId,bin_to_uuid(transmissionProfileId) as transmissionProfileId, bin_to_uuid(transmissionPostId) as transmissionPostId, transmissionContent, transmissionDateTime from transmission where transmissionPostId = bin_to_uuid(transmissionPostId) order by transmissionDateTime', {transmissionPostId})
-
-        // const mySqlQuery = 'select bin_to_uuid(transmissionId) as transmissionId,bin_to_uuid(transmissionProfileId) as transmissionProfileId, bin_to_uuid(transmissionPostId) as transmissionPostId, transmissionContent, transmissionDateTime from transmission where transmissionPostId = bin_to_uuid(transmissionPostId) order by transmissionDateTime'
-        // const [rows] = await mySqlConnection.execute(mySqlQuery, {transmissionPostId})
+        const [rows] = await mySqlConnection.execute('select bin_to_uuid(transmissionId) as transmissionId,bin_to_uuid(transmissionProfileId) as transmissionProfileId, bin_to_uuid(transmissionPostId) as transmissionPostId, transmissionContent, transmissionDateTime from transmission where transmissionPostId = uuid_to_bin(:transmissionPostId) order by transmissionDateTime', {transmissionPostId})
 
         return rows;
     } catch (error) {
