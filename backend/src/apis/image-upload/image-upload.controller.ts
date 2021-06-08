69ea038cff53dc0d5d3fd44bb60c11a855d30ff9
Imyraly Garcia <iarambula@cnm.edu>
2021-06-08 13:54:43 -0600

    Made changes to cloudinary utils and image controller

--- backend/src/apis/image-upload/image-upload.controller.ts
+++ backend/src/apis/image-upload/image-upload.controller.ts
@@ -4,8 +4,9 @@ import {uploadToCloudinary} from "../../utils/cloudinary.utils";
 export async function imageUploadController(request: Request, response: Response, nextFunction: NextFunction) {
     try {
         // uncomment if in production
-        // const message : string = await uploadToCloudinary(request)
-        const message : string =  "http://placekitten.com/150/150"
+        // @ts-ignore
+        const message : string = await uploadToCloudinary(request)
+        // const message : string =  "http://placekitten.com/150/150"
         return response.json({status: 200, data: null, message: message})
     } catch (error) {
         return response.json({status:400, message: error.message, data: null})
--- backend/src/apis/image-upload/image-upload.route.ts
+++ backend/src/apis/image-upload/image-upload.route.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
-
 import {imageUploader} from "../../utils/controllers/multer.controller";
-import {imageUploadController} from "./image-upload.contoller";
+import {imageUploadController} from "./image-upload.controller";
 
 export const ImageUploadRouter = Router();
 
