6564432d3aed48ea4d14a5704ba18ce5ff8f7e70
Jawaun Young <jyoung84@cnm.edu>
2021-06-09 14:43:05 -0700

    created profileSlice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,8 @@
+import profiles from "./profileSlice"
 import posts from "./postSlice"
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 
-const reducer =  combineReducers({posts})
+const reducer =  combineReducers({posts, profiles})
 
 export const store = configureStore({reducer})
+
