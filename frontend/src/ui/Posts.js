098c0e0cf141662c8d18065bb7607dee4d69b1f0
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-06-09 10:56:23 -0600

    set up redux and added posts example

--- frontend/src/ui/Posts.js
+++ frontend/src/ui/Posts.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import {CardColumns, Col, Container, Row} from "react-bootstrap";
 import {ThreadComponent} from "./components/ThreadComponent";
+//change misquotes to correct api
+import { fetchAllMisquotes } from '../store/misquote'
+import { PostCard } from './PostCard'
+import {useDispatch, useSelector} from "react-redux";
 
 export const Posts = () => {
+
+
+        // Tell this component that it needs to watch for items that live outside of this component.
+        // This is how we make sure this component looks for our data from Redux's call to the backend.
+        const dispatch = useDispatch()
+        const initialEffects = () => {
+            dispatch(fetchAllMisquotes())
+        }
+        React.useEffect(initialEffects, [dispatch])
+
+        // Render our misquotes constant - before we have our data, render the skeleton.
+        // After we have our data, render the full object with our data.
+        const misquotes = useSelector((state) => state.misquotes ? state.misquotes : [])
+
     return (
         <>
          <Container>
@@ -13,6 +31,11 @@ export const Posts = () => {
              </Row>
              <Row>
                  <ThreadComponent />
+             </Row>
+             <Row>
+                 <CardColumns className="p-4">
+                     {misquotes.map(misquote => <PostCard key={misquote.misquoteId} misquote={misquote} />)}
+                 </CardColumns>
              </Row>
          </Container>
 
