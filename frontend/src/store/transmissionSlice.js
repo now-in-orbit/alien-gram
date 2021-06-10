441efa147b559c9f93a1c528c0f91f1439a611e2
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-06-09 20:22:29 -0600

    created transmissionSlice

---
+++ frontend/src/store/transmissionSlice.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+import {createSlice} from '@reduxjs/toolkit'
+import {httpConfig} from "../utils/httpConfig"
+import {fetchProfileByProfileId} from './profileSlice';
+import {getAllPosts, getPostsByProfileId} from "./postSlice";
+
+const transmissionSlice = createSlice({
+    name: "transmissions",
+    initialState: [],
+    reducers: {
+        getAllTransmissions: (posts, action) => {
+            return action.payload
+        },
+        getTransmissionsByPostId: (posts, action) => {
+            return action.payload
+        },
+    },
+})
+
+// Make our actions callable as function getAllMisquotes.
+export const {getAllTransmissions, getTransmissionsByPostId} = transmissionSlice.actions
+
+// Create an export to allow async calls to our action
+export const fetchAllTransmissions = () => async dispatch => {
+    const {data} = await httpConfig(`/apis/transmission/`)
+    dispatch(getAllTransmissions(data))
+}
+
+export const fetchTransmissionsByTransmissionPostId = (transmissionPostId) => async dispatch => {
+    const {data} = await httpConfig(`/apis/transmission/transmissionPostId/${transmissionPostId}`);
+    dispatch(getTransmissionsByPostId(data))
+}
+
+export const fetchAllTransmissionsAndPosts = () => async (dispatch, getState) => {
+    await dispatch(fetchAllTransmissions())
+    const postIds = _.uniq(_.map(getState().transmissions, "transmissionPostId"));
+    postIds.forEach(postId => dispatch(fetchPostByPostId(postId)));
+}
+
+// We use export default here so that if something imports this file, they will get it by default
+export default postSlice.reducer
