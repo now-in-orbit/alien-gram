1ba3cca2a377f62ec96c03d47b492d313d441f09
Robert Inez <rinez@cnm.edu>
2021-06-09 22:03:25 -0600

    making progress on signin and sign up

--- frontend/src/ui/components/Navigation.js
+++ frontend/src/ui/components/Navigation.js
@@ -1,7 +1,35 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import {Nav, Navbar, NavDropdown} from "react-bootstrap"
 import './Navigation.css'
-export const  Navigation =  () => {
+import {fetchAuth} from "../../store/auth"
+import {SignInModal} from './main-nav/sign-in/SignInModal';
+import {SignUpModal} from './main-nav/sign-up/SignUpModal';
+import {useDispatch, useSelector} from 'react-redux';
+export const  Navigation =  (props) => {
+
+    const auth = useSelector(state => state.auth);
+    const dispatch = useDispatch()
+    const effects = () => {
+        dispatch(fetchAuth());
+    };
+    const inputs = [];
+    useEffect(effects, inputs);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // isModalOpen prevents the sign in modal being removed from the dom before the
+    // sign-in modal is closed by the user
+    const isModalOpen = ()=> {
+        if(!auth) {
+            return !auth
+        } else if(show === true && auth  ) {
+            return true
+        }
+    }
+
     return (
         <>
             <Navbar bg="dark" expand="lg" variant='dark'>
@@ -17,6 +45,12 @@ export const  Navigation =  () => {
                             <NavDropdown.Item href="/Transmissions">Transmissions</NavDropdown.Item>
                             <NavDropdown.Item href="/Likes">Likes</NavDropdown.Item>
                         </NavDropdown>
+                        {isModalOpen()  &&  (
+                            <>
+                                <SignUpModal/>
+                                <SignInModal show={show} handleClose={handleClose} handleShow={handleShow}/>
+                            </>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
