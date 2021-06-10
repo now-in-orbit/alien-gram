e3467e73dfacaa883b4223a6b47489d30ccd2ec8
Imyraly Garcia <iarambula@cnm.edu>
2021-06-09 21:54:21 -0600

    Created like component

--- frontend/src/ui/PostCard.js
+++ frontend/src/ui/PostCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import {useSelector} from 'react-redux';
+import {httpConfig} from "../utils/httpConfig";
+import {getAllPosts} from "../store/postSlice";
+import {useDispatch} from "react-redux";
 
 
 export function PostCard(props) {
@@ -19,25 +22,41 @@ export function PostCard(props) {
         const profile = profiles.find(profile => post.postProfileId === profile.profileId)
         return (
             <>
-                {profile && <img src = {profile.profileAvatarUrl} alt = 'Avatar' />}
+                {profile && <img src={profile.profileAvatarUrl} alt='Avatar'/>}
             </>
         )
     }
 
     const {post} = props
 
+    const dispatch = useDispatch()
+
+    const clickLike = () => {
+        httpConfig.post("/apis/like/", {likePostId: post.postId})
+            .then(reply => {
+                    if (reply.status === 200) {
+                        console.log(reply)
+                        dispatch(getAllPosts())
+                    }
+                    console.log(reply)
+                }
+            );
+    }
+
+
     return (
         <>
             <Card className="card text-center">
                 <div className="card-body">
                     <div>
-                        <FindAvatarUrl/><FindUsername />
+                        <FindAvatarUrl/><FindUsername/>
                     </div>
                     <Card.Text>
                         <div>
                             {post.postContent}
                         </div>
                     </Card.Text>
+                    <button onClick={clickLike}>{post.likeCount}<span role="img" aria-label="thumbs up emoji">👍️</span></button>
                 </div>
             </Card>
         </>
