feb7676c5c3a17df798a18f8c68bb110dd8fd109
Imyraly Garcia <iarambula@cnm.edu>
2021-06-11 13:46:35 -0600

    Created seed data

--- sql/statements.sql
+++ sql/statements.sql
@@ -1,4 +1,3 @@
-
 insert into sighting(sightingId, sightingCity, sightingSummary, sightingDateTime) values (uuid_to_bin(uuid()), 'Albuquerque', 'I saw a little green man eat my dog', now())
 
 select bin_to_uuid(postId) as postId, bin_to_uuid(postProfileId) as postProfileId, postContent, postDate from post where postProfileId = uuid_to_bin('dcbf0a01-c726-11eb-aad0-0242ac160002') order by postDate desc
@@ -7,3 +6,8 @@ DROP TABLE IF EXISTS sighting;
 
 
 insert into post(postId, postProfileId, postContent, postDate) values (uuid_to_bin(uuid()), uuid_to_bin('c09a5b7a-ca08-11eb-827a-0242ac1d0002'), 'I saw a little green man eat my dog', now())
+
+
+
+
+
