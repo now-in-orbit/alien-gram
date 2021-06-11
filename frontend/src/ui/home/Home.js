e5f9e1e9376500eb3acf393d220afbe609cd7d8c
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-06-11 06:32:36 -0600

    removed extra line on home

--- frontend/src/ui/home/Home.js
+++ frontend/src/ui/home/Home.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Welcome } from "./Welcome"
 import {Posts} from "./Posts";
 
-import {TransmissionComponent} from "../shared/components/transmissions/TransmissionComponent";
+
 
 
 
@@ -12,7 +12,6 @@ export const Home = () => {
 		<>
 			<Welcome />
 			<Posts />
-			<TransmissionComponent />
 		</>
 	)
 }
