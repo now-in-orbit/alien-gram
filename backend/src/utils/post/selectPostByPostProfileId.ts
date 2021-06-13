439ce1025acfce526c07ce30ffdaaaf46f9a8242
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-06-13 10:51:22 -0600

    fixed like bug

--- backend/src/utils/post/selectPostByPostProfileId.ts
+++ backend/src/utils/post/selectPostByPostProfileId.ts
@@ -5,7 +5,7 @@ export const selectPostByPostProfileId = async (postProfileId: string) => {
 	try {
 		const mysqlConnection = await connect();
 
-		const [rows] = await mysqlConnection.execute('select bin_to_uuid(postId) as postId, bin_to_uuid(postProfileId) as postProfileId, postContent, postDate from post where postProfileId = uuid_to_bin(:postProfileId)', {postProfileId})
+		const [rows] = await mysqlConnection.execute('select bin_to_uuid(postId) as postId, bin_to_uuid(postProfileId) as postProfileId, postContent, postDate, (SELECT COUNT(likePostId) FROM `like` WHERE post.postId = like.likePostId) AS likeCount from post INNER JOIN profile ON profile.profileId = post.postProfileId where postProfileId = uuid_to_bin(:postProfileId)', {postProfileId})
 
 		// @ts-ignore
 		return rows;
